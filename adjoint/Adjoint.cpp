8bd2cad99fb2e67d155f1342fab265877e8fe181
Sven Hingst <sven.hingst@tum.de>
2022-06-12 20:27:13 +0200

    cleaned up Adjoint.cpp

--- adjoint/Adjoint.cpp
+++ adjoint/Adjoint.cpp
@@ -16,9 +16,9 @@
 
 
 tarch::logging::Log earthadj::Adjoint::_log("earthadj::Adjoint");
-#define SIMPLE
-//#define HEL
-//#define ONLYPWAVES
+#define SIMPLE   //TWO Halfspaces scenario
+//#define HEL	//Helsinki scenario
+//#define ONLYPWAVES	//only P-waves scenario
 
 
 
@@ -50,52 +50,15 @@ void earthadj::Adjoint::adjustPointSolution(const double *const x, const double 
 		auto xx = x[0];
 		auto yy = x[1];
 
-		double probex=9.0;
-		double probey=13.0;
+		double probex=9.0; //TODO set x-cordinate of the point of interest
+		double probey=13.0; //TODO set y-cordinate of the point of interest
 
-//		if ((xx < 8.5 && xx > 7.5) && (yy < 11.0 && yy > 9.0)) {
 		auto dir = std::exp(-(xx-probex)*(xx-probex)*4-(yy-probey)*(yy-probey)*4)/(4*M_PI);
-//		auto dir = std::exp(-1.0*std::abs(1-(xx-probex)*(xx-probex)-(yy-probey)*(yy-probey)));
-//		Q[sigma11] = dir;//dir*-125.153;
-//		Q[sigma22] = dir;//80.77*dir;
-//		Q[sigma12] = dir;//-28.6597*dir;
-//
-//		Q[uu]=0.0;//TODO !!!!!!!!
-//		Q[vv]=0.0;
-//		Q[sigma11] = dir;//dir*-125.153;
-//		Q[sigma22] = dir;//80.77*dir;
-		Q[sigma11] = 0.0;//dir*-125.153;
-		Q[sigma22] = 0.0;//80.77*dir;
-		Q[sigma12] = dir;//-28.6597*dir;
-
-//		Q[uu]=-dir*3/5.83095;//dir;//TODO cutoff direc delata?
-//		Q[vv]=dir*5/5.83095;//dir;
-//		Q[uu]=dir;//TODO cutoff direc delata?
-//		Q[vv]=dir;
-		Q[uu]=0.0;//TODO cutoff direc delata?
+		Q[sigma12] = dir; //set for beta adjoint
+		Q[sigma11] = 0.0; //set for alpha adjoint
+		Q[sigma22] = 0.0; //set for alpha adjoint
+		Q[uu]=0.0;
 		Q[vv]=0.0;
-//		} else {
-//			Q[sigma11] = 0.0;
-//			Q[sigma22] = 0.0;
-//			Q[sigma12] = 0.0;
-//			Q[uu]=0.0;
-//			Q[vv]=0.0;
-//		}
-
-//    Q[uu] = std::exp(-(xx-5)*(xx-5)-(yy-5)*(yy-5));
-//    Q[vv] = std::exp(-(xx-5)*(xx-5)-(yy-5)*(yy-5));;
-//		Q[uu] = 0.0;//-86.857*dir;
-//		Q[vv] = 0.0;//25.2797*dir;
-//		if (xx < 7.5) {
-//			Q[lamb] = 2.0;
-//			Q[mu] = 0.5;
-//		} else {
-//			Q[lamb] = 2.0;
-//			Q[mu] = 2.0;
-//		}
-//		Q[rho] = 1.0;
-//		WP1(Q,xx,yy);
-//		second_example(Q,xx,yy);
 #ifdef HEL
 		vsp_helsinki(Q,xx,yy);
 #endif
@@ -194,27 +157,5 @@ void earthadj::Adjoint::flux(const double *const Q, double **const F) {
 	F[1][vv] = (Q[lamb] + 2 * Q[mu]) * Q[sigma22] + Q[lamb] * Q[sigma11];
 
 }
-
-/*
-void earthadj::Adjoint::initPointSourceLocations(const std::vector<std::string> &cmdlineargs,
-												 const exahype::parser::ParserView &constants) {
-	pointSourceLocation[0][0]=3.0;
-	pointSourceLocation[0][1]=4.0;
-}
-
-
-void earthadj::Adjoint::pointSource(const double *const Q, const double *const x, const double t, const double dt,
-									double *const forceVector, int n) {
-	double T=0.1;
-	double M_0=1e4;
-//	auto M_xy=(M_0*t/(T*T))*std::exp(-t*10/T);
-	auto M_xy=M_0;
-	forceVector[sigma12]=M_xy;
-	forceVector[sigma11]=M_xy;
-	forceVector[sigma22]=M_xy;
-	forceVector[uu]=0.0;
-	forceVector[vv]=0.0;
-}*/
-
 
 
